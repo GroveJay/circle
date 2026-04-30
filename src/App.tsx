f8cd4b925214f3de6ce69c9ea3d741cba46d2965
John Grover <johnrichardgrover@gmail.com>
2026-04-29 22:47:23 -0700

    Circularity

--- src/App.tsx
+++ src/App.tsx
@@ -16,56 +16,84 @@ const detectImageCircles = async (
   upDiff: cv.Scalar,
 ) => {
   cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY, 0);
-
   let circles = new cv.Mat();
   cv.HoughCircles(gray, circles, cv.HOUGH_GRADIENT, dp, min_dist, param1, param2, min_radius, max_radius);
+
+  const circlesCount = circles.cols;
+  let relevantContourCircularity = 0;
+
+  if (circlesCount !== 1) {
+    return {
+      circlesCount,
+      relevantContourCircularity,
+    }
+  }
+
   cv.medianBlur(gray, gray, 5)
   cv.adaptiveThreshold(gray, gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2);
 
-  let color = new cv.Scalar(255, 0, 0);
   let mask = new cv.Mat();
   for (let i = 0; i < circles.cols; ++i) {
     let x = circles.data32F[i * 3];
     let y = circles.data32F[i * 3 + 1];
-    let radius = circles.data32F[i * 3 + 2];
     let center = new cv.Point(x, y);
-    cv.circle(img, center, radius, color, 10);
     cv.floodFill(gray, mask, center, new cv.Scalar(0), new cv.Rect(), loDiff, upDiff, cv.FLOODFILL_FIXED_RANGE); 
   }
   mask.delete();
 
   let contours = new cv.MatVector();
   let hierarchy = new cv.Mat();
-  let graycolor = new cv.Scalar(128);
+  cv.findContours(gray, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_NONE);
 
-  cv.findContours(gray, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE);
-  const circlesCount = circles.cols;
-  let relevantContourRoundness = 0;
-  if (circlesCount === 1) {
-    const maxArea = Math.PI * Math.pow(max_radius, 2);
-    const onlyCircleCenter = new cv.Point(
-      circles.data32F[0],
-      circles.data32F[1]
-    );
-    
-    for (let i = 0; i < (contours.size() as unknown as number); i++) {
-      const contour = contours.get(i);
-      if (cv.pointPolygonTest(contour, onlyCircleCenter, false) > 0) {
-        const area = cv.contourArea(contour);
-        if (area > maxArea) {
-          continue;
+  const maxArea = Math.PI * Math.pow(max_radius, 2);
+  const onlyCircleCenter = new cv.Point(
+    circles.data32F[0],
+    circles.data32F[1]
+  );
+  
+  for (let i = 0; i < (contours.size() as unknown as number); i++) {
+    const contour = contours.get(i);
+    if (cv.pointPolygonTest(contour, onlyCircleCenter, false) > 0) {
+      const area = cv.contourArea(contour);
+      if (area > maxArea) {
+        continue;
+      }
+      const permiter = cv.arcLength(contour, true);
+      const roundness = FOUR_PI * (area / Math.pow(permiter, 2));
+      if (roundness > 0.5) {
+        const contourMoments = cv.moments(contour);
+        const centroidX = contourMoments['m10'] / contourMoments['m00'];
+        const centroidY = contourMoments['m01'] / contourMoments['m00'];
+        const contourDistances = [];
+        let totalDistances = 0;
+        for (var t = 0; t < contour.data32S.length; t+=2) {
+            const x = contour.data32S[t];
+            const y = contour.data32S[t+1];
+            const distance = Math.sqrt(
+                Math.pow(
+                    x - centroidX,
+                    2
+                ) + Math.pow(
+                    y - centroidY,
+                    2
+                )
+            );
+            totalDistances += distance;
+            contourDistances.push(distance);
         }
-        // const convexHull = new cv.Mat();
-        // cv.convexHull(contour, convexHull);
-        cv.drawContours(gray, contours, i, graycolor, 3, cv.LINE_8, hierarchy, 100);
-        const permiter = cv.arcLength(contour, true);
-        const roundness = ((FOUR_PI * area) / Math.pow(permiter, 2));
-        if (roundness > 0.5) {
-          cv.drawContours(gray, contours, i, graycolor, 3, cv.LINE_8, hierarchy, 100);
-          relevantContourRoundness = roundness
-          break;
+        const averageDistance = totalDistances / contourDistances.length;
+        console.log('averageDistance', averageDistance);
+        let totalSquaredDistances = 0;
+        for (var j = 0; j < contourDistances.length; j++) {
+            totalSquaredDistances += Math.pow((contourDistances[j] - averageDistance), 2);
         }
-        // convexHull.delete();
+        console.log('totalSquaredDistances', totalSquaredDistances);
+        const sd = Math.sqrt(totalSquaredDistances / contourDistances.length);
+        console.log('sd', sd);
+        const circularity = sd / averageDistance;
+        console.log(circularity);
+        relevantContourCircularity = 1 - circularity;
+        break;
       }
     }
   }
@@ -75,27 +103,30 @@ const detectImageCircles = async (
   circles.delete();
   return {
     circlesCount,
-    relevantContourRoundness,
+    relevantContourCircularity,
   }
 }
 
 function App() {
   const [initialized, setInitialized] = useState(false);
   const [paused, setPaused] = useState(false);
-  // const [circles, setCircles] = useState(0);
-  const [roundness, setRoundness] = useState(0);
+  const [showSettings, setShowSettings] = useState(false);
+  const [circles, setCircles] = useState(0);
+  const [circularity, setCircularity] = useState(0);
   const [maxRoundnessSeen, setMaxRoundnessSeen] = useState(0);
   const [neighborDistance, setNeighborDistance] = useState(250);
-  const [param1,setParam1] = useState(75);
-  const [param2, setParam2] = useState(50);
+  const [param1,setParam1] = useState(40);
+  const [param2, setParam2] = useState(35);
   const [minRadius,setMinRadius] = useState(95);
-  const [maxRadius,setMaxRadius] = useState(144);
+  const [maxRadius,setMaxRadius] = useState(145);
   const [loDiff, setLoDiff] = useState(0);
   const [upDiff, setUpDiff] = useState(0);
 
   const videoInputRef = useRef<HTMLVideoElement>(null);
   const circledImageRef = useRef<HTMLCanvasElement>(null);
   const source = useRef<cv.Mat>(null);
+  const gray = useRef<cv.Mat>(null);
+
   const capture = useRef<cv.VideoCapture>(null);
 
   const detectCircles = useCallback(async () => {
@@ -108,27 +139,31 @@ function App() {
       return;
     }
     capture.current.read(source.current);
-    let gray = new cv.Mat();
-    const { relevantContourRoundness } = await detectImageCircles(
-      source.current, gray, 1, neighborDistance, param1, param2, minRadius, maxRadius,
+    
+    const { circlesCount, relevantContourCircularity } = await detectImageCircles(
+      source.current, gray.current, 1, neighborDistance, param1, param2, minRadius, maxRadius,
       new cv.Scalar(loDiff), new cv.Scalar(upDiff),
     );
-    // setCircles(circlesCount);
-    setRoundness(relevantContourRoundness);
+    setCircles(circlesCount);
+    setCircularity(relevantContourCircularity);
     setMaxRoundnessSeen((previousMaxRoundnessSeen) => {
-      if (relevantContourRoundness > previousMaxRoundnessSeen) {
-        return relevantContourRoundness;
+      if (relevantContourCircularity > previousMaxRoundnessSeen) {
+        return relevantContourCircularity;
       }
       return previousMaxRoundnessSeen;
     });
-    cv.circle(gray, new cv.Point(gray.cols / 2, gray.rows / 2), maxRadius - 5, new cv.Scalar(25), 10);
-    // cv.imshow(circledImageRef.current, circledImage);
-    cv.imshow(circledImageRef.current, gray);
-    gray.delete();
+
+    cv.circle(gray.current, new cv.Point(circledImageRef.current.width / 2, circledImageRef.current.height / 2), maxRadius, new cv.Scalar(128), 3);
+    try {
+      cv.imshow(circledImageRef.current, gray.current);
+    } catch (e) {
+      console.warn('caught exception: ', e);
+    }
+    
   }, [neighborDistance, param1, param2, minRadius, maxRadius]);
 
   const initialize = useCallback(async () => {
-    if (!videoInputRef || !videoInputRef.current) return;
+    if (!videoInputRef || !videoInputRef.current || initialized) return;
     let stream = undefined;
     try {
       stream = await navigator.mediaDevices.getUserMedia({
@@ -136,11 +171,20 @@ function App() {
           facingMode: {
             exact: "environment",
           },
+          width: { min: 1280, max: 1560 },
+          height: { min: 720, max: 1440 }
         },
         audio: false,
       });
     } catch {
-      stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+      stream = await navigator.mediaDevices.getUserMedia(
+        {
+          video: true,/*{
+             width: { min: 1280, max: 1560 },
+             height: { min: 720, max: 1440 }
+          },*/
+          audio: false,
+        });
     }
     if (!stream) {
       console.log("An error occurred!");
@@ -154,10 +198,15 @@ function App() {
     const height = videoInputRef.current.videoHeight;
     const width = videoInputRef.current.videoWidth;
     source.current = new cv.Mat(height, width, cv.CV_8UC4);
+    gray.current = new cv.Mat();
     capture.current = new cv.VideoCapture(videoInputRef.current)
-    
+
+    const smallerDimension = Math.min(width, height);
+    setMinRadius((smallerDimension * .6) / 2);
+    setMaxRadius((smallerDimension * .8) / 2);
+    setNeighborDistance(smallerDimension / 2);
     setInitialized(true);
-  }, [videoInputRef.current]);
+  }, [videoInputRef.current, initialized]);
 
   useEffect(() => {
     if (cv.getBuildInformation) { 
@@ -188,7 +237,8 @@ function App() {
     <div className="App">
       <video className="videoInput" ref={videoInputRef} />
       <canvas className="outputImage" ref={circledImageRef} />
-      <h4>Circle Roundness {(roundness * 100).toFixed(2)} %</h4>
+      <h4>Circles Count: {circles}</h4>
+      <h4>Circle Circularity {(circularity * 100).toFixed(2)} %</h4>
       <h4>Max Roundness {(maxRoundnessSeen * 100).toFixed(2)} %</h4>
       <div className="settings">
         <div>
@@ -200,22 +250,28 @@ function App() {
             Reset Max Roundness
           </button>
           <button
-            style={{ display: 'none' }}
             onClick={() => {
-              setPaused(!paused);
+              setShowSettings(!showSettings);
             }}
           >
-            {paused ? 'Unpause' : 'Pause'}
+            ?
           </button>
         </div>
         <div style={{
-          display: 'none',
+          display: showSettings ? '' : 'none',
           flexDirection: 'row',
           maxWidth: '600px',
           margin: '0 auto',
           flexWrap: 'wrap',
           justifyContent: 'space-evenly'
         }}>
+          <button
+            onClick={() => {
+              setPaused(!paused);
+            }}
+          >
+            {paused ? 'Unpause' : 'Pause'}
+          </button>
           <InputSlider
             description="Neighbor Distance"
             value={neighborDistance}
